784da7c62f32228096c05dbe9020e212d209371f
Turkanbozdag <64183806+Turkanbozdag@users.noreply.github.com>
2020-06-28 15:07:22 -0400

    Update TRY.java

--- src/TEST11/TRY.java
+++ src/TEST11/TRY.java
@@ -4,7 +4,8 @@ public class TRY {
     public static void main(String[] args) {
         System.out.println("Hellooooooo");
         System.out.println("Life is :))))))))))))))))");
-
+        System.out.println("hayat guzel");
+            
 
 
     }
