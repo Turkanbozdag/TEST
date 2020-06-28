c41ce8eaec838d8124e393c0efbce9251e6c509d
TurkanBozdag <turkanbozdag@yahoo.com>
2020-06-28 15:05:00 -0400

    commit

--- src/TEST11/TRY.java
+++ src/TEST11/TRY.java
@@ -3,6 +3,7 @@ package TEST11;
 public class TRY {
     public static void main(String[] args) {
         System.out.println("Hellooooooo");
+        System.out.println("Life is :))))))))))))))))");
 
 
 
